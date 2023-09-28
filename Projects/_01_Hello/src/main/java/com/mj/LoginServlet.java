64e69b2dd4683d60a58362f541a69ba4174a6b43
whj <wanghaijun.425@163.com>
2023-09-28 15:43:18 +0800

    add:Servlet的基本使用-接收参数 和 返回响应数据

--- Projects/_01_Hello/src/main/java/com/mj/LoginServlet.java
+++ Projects/_01_Hello/src/main/java/com/mj/LoginServlet.java
@@ -18,7 +18,11 @@ import java.io.IOException;
 @WebServlet("/login") //请求的路劲 ps / 不能少。
 public class LoginServlet extends HttpServlet {
     /**
-     * 当客户端请求的是 Get 请求， 就会调用 HttpServlet 的doGet方法。
+     *  当客户端请求的是 Get 请求， 就会调用 HttpServlet 的doGet方法。
+     * @param request 用来 接收客户端发来的数据
+     * @param response 用来给客户端 发送响应
+     * @throws ServletException
+     * @throws IOException
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -30,6 +34,21 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //super.doPost(req, resp); PS不要调用 super 否则报 405
         System.out.println("来请求了");
+
+        //1.取出请求的参数 option + cmd +v 快速生成局部变量（默认的快捷键）
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println("参数：" + username + "_" + password);
+
+        // ctr + d : debug 快捷键
+        // 快速弹出 Edit Config 弹框 ： 自己配置的是 Shift + e 。
+        //2:判断用户名，密码是否正确（假设都是 123 成功）
+        if ("123".equals(username) && "123".equals(password)){
+            // 给客户端 发送数据
+            response.getWriter().write("Login Success!");
+        } else {
+            response.getWriter().write("Login Failure!");
+        }
 
     }
 }
